20c5b28d517ebedc719d94b96b993978a4107783
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-09-01 22:58:23 +0300

    added third tab

--- src/js/main.js
+++ src/js/main.js
@@ -8,8 +8,18 @@ $(function(){
 
     $(this).addClass('tab--active');
     $($(this).attr('href')).addClass('tabs-content--active');
-    $('.slider-inner').slick('refresh');
-    
+    $('.slider-inner, .faction-slider').slick('refresh');
+  });
+  
+  $('.organization__item').on('click', function(e){
+    e.preventDefault();
+
+    $($(this).siblings()).removeClass('organization__item--active');
+    $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('faction__content--active');
+
+    $(this).addClass('organization__item--active');
+    $($(this).attr('href')).addClass('faction__content--active');
+    $('.faction-slider').slick('refresh');
   });
 
   $('.slider-inner').slick({
@@ -20,8 +30,8 @@ $(function(){
     nextArrow: '<button class="slider-arrows slider-arrows__left"><img src="images/icons/arrow-right.svg" alt=""></button>',
   });
 
-  $('.list__item-btn, .cars__list-btn').on('click', function(){
-    $(this).toggleClass('list__item-btn--active, cars__list-btn--active');
+  $('.list__item-btn').on('click', function(){
+    $(this).toggleClass('list__item-btn--active');
   });
   
   document.querySelectorAll('.cars__info').forEach((item) =>
@@ -44,5 +54,13 @@ $(function(){
     
     })
   );
+
+  $('.faction-slider').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slider-arrows faction-arrows slider-arrows__right"><img src="images/icons/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="slider-arrows faction-arrows slider-arrows__left"><img src="images/icons/arrow-right.svg" alt=""></button>',
+  });
 
 });
